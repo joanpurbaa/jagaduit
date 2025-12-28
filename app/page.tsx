2066521080084c4a5cfef8cc8d1215fdd01dc8d3
joan purba <joanpurba562@gmail.com>
2025-12-29 06:54:35 +0700

    fix: fix upload api

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,8 @@ export default function Home() {
 	const [isLoading, setIsLoading] = useState(false);
 	const [progress, setProgress] = useState(0);
 
+  const baseUrl = process.env.PRODUCTION_URL || process.env.BASE_URL;
+
 	const uploadImage = () => {
 		if (image) {
 			setIsLoading(true);
@@ -35,7 +37,7 @@ export default function Home() {
 			const formData = new FormData();
 			formData.append("image", image);
 
-			fetch("/api/upload", {
+			fetch(`${baseUrl}/api/upload`, {
 				method: "POST",
 				body: formData,
 			})
