ceb18ad098b4b6276567136928398829854b2473
joan purba <joanpurba562@gmail.com>
2025-12-29 06:00:08 +0700

    fix: fix base url

--- actions/register.ts
+++ actions/register.ts
@@ -5,13 +5,14 @@ import { redirect } from "next/navigation";
 export default async function RegisterAction(
 	registerCredentials: RegisterCredentialsType
 ) {
+  const baseUrl = process.env.NEXT_PUBLIC_APP_URL || process.env.BASE_URL;
 	const formData = new FormData();
 
 	formData.append("email", registerCredentials.email);
 	formData.append("username", registerCredentials.username);
 	formData.append("password", registerCredentials.password);
 
-	await fetch(`http://localhost:3000/api/register`, {
+	await fetch(`${baseUrl}/api/register`, {
 		method: "POST",
 		body: formData,
 	});
